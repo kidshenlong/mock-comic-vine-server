02029217f34ad421e4dfb611477540ac7d5220ff
kidshenlong <michaelpm91@googlemail.com>
2015-08-29 21:51:15 +0100

    New pagination fixtures. Change to logic to allow pagination.

--- server.js
+++ server.js
@@ -15,12 +15,30 @@ app.get('/', function (req, res) {
 
 app.get('/api/search', function (req, res) {
     console.log('hit search');
-    res.json(getJSONFixture('search'));
+    if(req.query.force){
+        res.json(getJSONFixture(req.query.force));
+    }else{
+        res.json(getJSONFixture('search'));
+    }
 });
 
 app.get('/api/issues', function (req, res) {
     console.log('hit issues search');
-    res.json(getJSONFixture('search_issues'));
+    if(req.query.force){
+        res.json(getJSONFixture(req.query.force));
+    }else {
+        var limit = 20;
+        var page = req.query.page;
+        if(page > 2){
+            res.json(getJSONFixture('search_issues_' + page));
+        }else if(page == 2){
+            var json = getJSONFixture('search_issues_out_of_bounds')
+            json.offset = offset;
+            res.json(json);
+        }else{
+            res.json(getJSONFixture('search_issues'));
+        }
+    }
 });
 
 var server = app.listen(3000, function () {
